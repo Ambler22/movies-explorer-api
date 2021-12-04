6074e554f1cc193d22a055e80101abc04ce8af29
Roman Drozdov <hamilion.91@mail.ru>
2021-12-04 15:57:01 +0300

    chang for review

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(requestLogger);
-app.use(limiter);
 
 app.use(router);
 
