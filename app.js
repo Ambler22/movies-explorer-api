a853bae4e35864dec4201bb1d0a80807510e5059
Roman Drozdov <hamilion.91@mail.ru>
2022-02-15 16:47:26 +0300

    cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { router } = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -19,12 +19,14 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 });
 
 const CORS_WHITELIST = [
+  'localhost:3000',
   'http://localhost:3000',
   'https://movies.ex.nomoredomains.rocks',
   'http://movies.ex.nomoredomains.rocks',
+  'http://api.movies.ex.nomoredomains.rocks',
 ];
 
-const corsOption = {
+/* const corsOption = {
   credentials: true,
   optionsSuccessStatus: 204,
   origin: function checkCorsList(origin, callback) {
@@ -36,7 +38,27 @@ const corsOption = {
   },
 };
 
-app.use('*', cors(corsOption));
+app.use('*', cors(corsOption)); */
+
+app.use((req, res, next) => {
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (CORS_WHITELIST.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).send();
+  }
+
+  return next();
+});
 
 app.use(requestLogger);
 app.use(limiter);
